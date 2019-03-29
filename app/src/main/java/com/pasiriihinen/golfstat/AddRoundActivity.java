80f068e70f95cbdcb72424b204a8843dca709f21
pasiriihinen <pasiriihinen@gmail.com>
2019-03-29 12:36:20 +0100

    New table code and started to test addroundactivity.

--- app/src/main/java/com/pasiriihinen/golfstat/AddRoundActivity.java
+++ app/src/main/java/com/pasiriihinen/golfstat/AddRoundActivity.java
@@ -2,16 +2,27 @@ package com.pasiriihinen.golfstat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AddRoundActivity extends AppCompatActivity {
     //Add variables
+    DatabaseHelper myDb;
     TextView TextViewSelectCourse;
+    Button button_NextHole;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_round);
+
+        //DatabaseHelper
+        myDb = new DatabaseHelper(this);
+
+        //Assign the variables via findViewById
+        button_NextHole = (Button)findViewById(R.id.button_NextHole);
 
 
         TextViewSelectCourse = (TextView) findViewById(R.id.selectedCourseTextView);
@@ -20,5 +31,22 @@ public class AddRoundActivity extends AppCompatActivity {
             TextViewSelectCourse.setText(bundle.getString("CourseName") + "(" + bundle.getString("CourseId") + ")");
         }
 
+        PostHoleData();
+
+    }
+
+    //Method to post hole data to database
+    public void PostHoleData() {
+        button_NextHole.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean postedHoleData = myDb.postHoleData(3);
+                    if (postedHoleData == true) {
+                        Toast.makeText(AddRoundActivity.this, "Data posted To DB", Toast.LENGTH_SHORT).show();
+                    }
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/pasiriihinen/golfstat/DatabaseHelper.java
+++ app/src/main/java/com/pasiriihinen/golfstat/DatabaseHelper.java
@@ -8,10 +8,24 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "courses.db";
+    //Courses table
     public static final String TABLE_NAME = "course_table";
     public static final String COL_ID = "_id";
     public static final String COL_NAME = "NAME";
     public static final String COL_PAR = "PAR";
+    //Rounds table
+    public static final String ROUNDS_TABLE = "rounds_table";
+    public static final String ROUND_ID = "id";
+    public static final String ROUND_DATE = "DATE";
+    public static final String ROUND_COURSE = "COURSE";
+    public static final String ROUND_HOLENUM = "HOLENUM";
+    public static final String ROUND_HOLEPAR = "HOLEPAR";
+    public static final String ROUND_HOLESCORE   = "HOLESCORE";
+    public static final String ROUND_HOLEPUTTS = "HOLEPUTTS";
+    public static final String ROUND_HOLENFW = "HOLEFW";
+    public static final String ROUND_HOLECHIP = "HOLECHIP";
+    public static final String ROUND_HOLEPENALTY = "HOLEPENALTY";
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -21,6 +35,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_NAME +" (_id INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, PAR INTEGER)");
+        //Create rounds table
+        db.execSQL("create table " + ROUNDS_TABLE + "(id INTEGER PRIMARY KEY, DATE TEXT, COURSE TEXT, HOLENUM INTEGER, HOLEPAR INTEGER, HOLESCORE INTEGER, HOLEPUTTS INTEGER, HOLEFW TEXT, HOLECHIP TEXT, HOLEPENALTY TEXT)");
+
     }
 
     @Override
@@ -35,6 +52,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         contentValues.put(COL_NAME, name);
         contentValues.put(COL_PAR, par);
         long result = db.insert(TABLE_NAME, null, contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    //Post score for one hole
+    public boolean postHoleData(Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ROUND_ID, id);
+        long result = db.insert(ROUNDS_TABLE, null, contentValues);
         if(result == -1)
             return false;
         else
